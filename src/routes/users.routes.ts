ef87d30b2a073f906bc7cff00d1e14c4b2c96118
depaulaluis <henriquesilvadepaula@gmail.com>
2021-01-14 09:12:07 -0300

    Adicionando Upload de imagem de avatar em pasta temporaria 'tmp'

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUsersService from '../services/CreateUsersService';
 
-import ensureAuthenticated from '../middlewares/ensureAuthenticated'
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post ('/', async (request, response) => {
   try{
@@ -32,7 +35,8 @@ usersRouter.post ('/', async (request, response) => {
   }
 });
 
-usersRouter.patch ('/avatar', ensureAuthenticated, async (request, response)=> {
+usersRouter.patch ('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response)=> {
+
   return response.json({ ok: true});
 })
 
